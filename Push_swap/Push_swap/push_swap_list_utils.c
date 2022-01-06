c193f18298d66f2eb02718930841761155065f09
Jervex-e <94312066+Jerv-exe@users.noreply.github.com>
2022-01-06 12:00:24 +0100

    Add files via upload

---
+++ Push_swap/Push_swap/push_swap_list_utils.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_list_utils.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-asri <ael-asri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/13 15:09:11 by ael-asri          #+#    #+#             */
+/*   Updated: 2022/01/05 12:18:02 by ael-asri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack	*ft_lstnew(int content)
+{
+	t_stack	*ptr;
+
+	ptr = malloc(sizeof(t_stack));
+	if (ptr == NULL)
+		return (0);
+	ptr->data = content;
+	ptr->next = NULL;
+	return (ptr);
+}
+
+t_stack	*ft_lstlast(t_stack *lst)
+{
+	while (lst != NULL && lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
+
+void	ft_lstadd_front(t_stack **lst, t_stack *new)
+{
+	new->next = *lst;
+	*lst = new;
+}
+
+void	ft_lstadd_back(t_stack **lst, t_stack *new)
+{
+	t_stack	*list;
+
+	list = *lst;
+	if (*lst == 0)
+	{
+		*lst = new;
+		return ;
+	}
+	while (list->next != NULL)
+	{
+		list = list->next;
+	}
+	list->next = new;
+}
+
+int	ft_lstsize(t_stack *lst)
+{
+	int	count;
+
+	count = 0;
+	while (lst != NULL)
+	{
+		count++;
+		lst = lst->next;
+	}
+	return (count);
+}
+
+int	ft_atoi(char	*str)
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		else if (str[i] == '+')
+			sign = 1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (res * sign);
+}
